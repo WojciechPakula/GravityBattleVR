168cf5cb48ddcb0a1be77e4e30657e324df17692
WojciechPakula <wojciechpaku@gmail.com>
2018-12-01 19:38:44 +0100

    Sceny i menu testowe
    Trzeba do każdej sceny dodać obsługę urządzenia.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadHTCvive()
+    {
+        SceneManager.LoadScene("HTCvive_test");
+    }
+
+    public void load3Dvision()
+    {
+        SceneManager.LoadScene("3Dvision_test");
+    }
+}
